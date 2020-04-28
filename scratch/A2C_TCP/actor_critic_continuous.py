3ffd287fb6b8c98e6987fc5abaec49ee8b1e205e
AtomAnu <a.anuchitanukul@gmail.com>
2020-04-28 22:33:15 +0100

    Restructure Code

--- scratch/A2C_TCP/actor_critic_continuous.py
+++ scratch/A2C_TCP/actor_critic_continuous.py
@@ -28,54 +28,6 @@ class GenericNetwork(nn.Module):
 
         return x
 
-class ComplexNetwork(nn.Module):
-    def __init__(self, alpha, input_dims, fc1_dims, fc2_dims,
-                 n_actions):
-        super(ComplexNetwork, self).__init__()
-        self.input_dims = input_dims
-        self.fc1_dims = fc1_dims
-        self.fc2_dims = fc2_dims
-        self.n_actions = n_actions
-        self.fc1 = nn.Linear(*self.input_dims, self.fc1_dims)
-        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
-        self.fc3 = nn.Linear(self.fc2_dims, self.n_actions)
-        self.optimizer = optim.Adam(self.parameters(), lr=alpha)
-        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu:0')
-        self.to(self.device)
-
-    def forward(self, observation):
-        state = T.tensor(observation, dtype=T.float).to(self.device)
-        x = F.relu(self.fc1(state))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-
-        return x
-
-class ActorCriticNetwork(nn.Module):
-    def __init__(self, alpha, input_dims, fc1_dims, fc2_dims,
-                 n_actions):
-        super(ActorCriticNetwork, self).__init__()
-        self.input_dims = input_dims
-        self.fc1_dims = fc1_dims
-        self.fc2_dims = fc2_dims
-        self.n_actions = n_actions
-        self.fc1 = nn.Linear(*self.input_dims, self.fc1_dims)
-        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
-        self.pi = nn.Linear(self.fc2_dims, self.n_actions)
-        self.v = nn.Linear(self.fc2_dims, 1)
-        self.optimizer = optim.Adam(self.parameters(), lr=alpha)
-        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu:0')
-        self.to(self.device)
-
-    def forward(self, observation):
-        state = T.tensor(observation, dtype=T.float).to(self.device)
-        x = F.relu(self.fc1(state))
-        x = F.relu(self.fc2(x))
-        pi = self.pi(x)
-        v = self.v(x)
-
-        return pi, v
-
 class Agent(object):
     def __init__(self, alpha, beta, input_dims, gamma=0.99, n_actions=2,
                  layer1_size=256, layer2_size=256, n_outputs=1):
@@ -89,7 +41,6 @@ class Agent(object):
 
     def choose_action(self, observation):
         mu, sigma  = self.actor.forward(observation).to(self.actor.device)
-        # sigma = self.actor.forward(observation)  # .to(self.actor.device)
         mu += 2
         print(type(sigma))
         sigma = sigma.clamp(-10,10)
@@ -98,14 +49,12 @@ class Agent(object):
         sigma = T.exp(sigma)
         print("NEW Sigma: {}".format(sigma))
         action_probs = T.distributions.Normal(mu, sigma.float(), validate_args=True)
-        # action_probs = T.distributions.Exponential(rate=1/sigma, validate_args=True)
         probs = action_probs.rsample(sample_shape=T.Size([self.n_outputs]))
         self.log_probs = action_probs.log_prob(probs).to(self.actor.device)
-        # action = T.tanh(probs)
+
         action = probs
 
         return [round(action.item(),2)]
-        # return [round(action[0].item(),2),int(action[1].item())]
 
     def learn(self, state, reward, new_state, done):
         self.actor.optimizer.zero_grad()
@@ -113,59 +62,57 @@ class Agent(object):
         critic_value_ = self.critic.forward(new_state)
         critic_value = self.critic.forward(state)
         reward = T.tensor(reward, dtype=T.float).to(self.actor.device)
-        # delta = ((reward + self.gamma*critic_value_*(1-int(done))) -critic_value)
-        delta = ((reward + (self.gamma ** 200) * critic_value_ ) - critic_value)
+        delta = ((reward + self.gamma*critic_value_*(1-int(done))) -critic_value)
 
         actor_loss = -self.log_probs * delta
-        # actor_loss_1 = -self.log_probs[0] * delta * 0.5
-        # actor_loss_2 = -self.log_probs[1] * delta * 0.5
-        # actor_loss_1 = self.log_probs[0] * delta * 0.5
-        # actor_loss_2 = self.log_probs[1] * delta * 0.5
+
         critic_loss = delta**2
 
         (actor_loss + critic_loss).backward()
 
-        # self.actor.optimizer.zero_grad()
-        # (actor_loss).backward(retain_graph = True)
+
         self.actor.optimizer.step()
-        # self.critic.optimizer.zero_grad()
-        # # (actor_loss_1 + actor_loss_2 + critic_loss).backward()
-        # (critic_loss).backward()
         self.critic.optimizer.step()
 
-class NewAgent(object):
+class NStepAgent(object):
     def __init__(self, alpha, beta, input_dims, gamma=0.99, n_actions=2,
-                 layer1_size=256, layer2_size=256, n_outputs=2):
+                 layer1_size=256, layer2_size=256, n_outputs=1):
         self.gamma = gamma
         self.log_probs = None
         self.n_outputs = n_outputs
-        self.actor_critic = ActorCriticNetwork(alpha, input_dims, layer1_size,
-                                        layer2_size, n_actions=n_actions)
+        self.actor = GenericNetwork(alpha, input_dims, layer1_size,
+                                           layer2_size, n_actions=n_actions)
+        self.critic = GenericNetwork(beta, input_dims, layer1_size,
+                                            layer2_size, n_actions=1)
 
     def choose_action(self, observation):
-        pi, v = self.actor_critic.forward(observation)
-
-        mu, sigma = pi
+        mu, sigma  = self.actor.forward(observation).to(self.actor.device)
+        mu += 2
+        print(type(sigma))
+        sigma = sigma.clamp(-10,10)
+        print("Mu: {}".format(mu))
+        print("Sigma: {}".format(sigma))
         sigma = T.exp(sigma)
-        action_probs = T.distributions.Normal(mu, sigma)
-        probs = action_probs.sample(sample_shape=T.Size([self.n_outputs]))
-        self.log_probs = action_probs.log_prob(probs).to(self.actor_critic.device)
-        action = T.tanh(probs)
+        print("NEW Sigma: {}".format(sigma))
+        action_probs = T.distributions.Normal(mu, sigma.float(), validate_args=True)
+        probs = action_probs.rsample(sample_shape=T.Size([self.n_outputs]))
+        self.log_probs = action_probs.log_prob(probs).to(self.actor.device)
+        action = probs
 
-        return action.item()
+        return [round(action.item(),2)]
 
-    def learn(self, state, reward, new_state, done):
-        self.actor_critic.optimizer.zero_grad()
-
-        _, critic_value_ = self.actor_critic.forward(new_state)
-        _, critic_value = self.actor_critic.forward(state)
-
-        reward = T.tensor(reward, dtype=T.float).to(self.actor_critic.device)
-        delta = reward + self.gamma*critic_value_*(1-int(done)) - critic_value
+    def learn(self, state, reward, new_state, done, n):
+        self.actor.optimizer.zero_grad()
+        self.critic.optimizer.zero_grad()
+        critic_value_ = self.critic.forward(new_state)
+        critic_value = self.critic.forward(state)
+        reward = T.tensor(reward, dtype=T.float).to(self.actor.device)
+        delta = ((reward + (self.gamma ** n) * critic_value_ ) - critic_value)
 
         actor_loss = -self.log_probs * delta
         critic_loss = delta**2
 
         (actor_loss + critic_loss).backward()
 
-        self.actor_critic.optimizer.step()
+        self.actor.optimizer.step()
+        self.critic.optimizer.step()
---
+++ scratch/A2C_TCP/td0_agent_train.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#/usr/bin/python3
+
+import argparse
+from ns3gym import ns3env
+from tcp_base import TcpTimeBased
+from tcp_newreno import TcpNewReno
+# from actor_critic_discrete import Agent
+from actor_critic_continuous import Agent
+from utils import plotLearning
+
+import math
+import csv
+import matplotlib.pyplot as plt
+import collections
+
+parser = argparse.ArgumentParser(description='Start simulation script on/off')
+parser.add_argument('--start',
+                    type=int,
+                    default=1,
+                    help='Start ns-3 simulation script 0/1, Default: 1')
+parser.add_argument('--iterations',
+                    type=int,
+                    default=1,
+                    help='Number of iterations, Default: 1')
+args = parser.parse_args()
+startSim = bool(args.start)
+iterationNum = 100
+
+port = 5557
+simTime = 100 # seconds
+seed = 12
+simArgs = {"--duration": simTime,}
+debug = False
+
+env = ns3env.Ns3Env(port=port, startSim=startSim, simSeed=seed, simArgs=simArgs, debug=debug)
+
+env.reset()
+
+ob_space = env.observation_space
+ac_space = env.action_space
+
+
+
+print("Observation space: ", ob_space,  ob_space.dtype)
+print("Action space: ", ac_space, ac_space.dtype)
+
+a2c_agent = Agent(alpha=5.19e-5, beta=2.42e-5, input_dims=[5], gamma=0.99,
+                  n_actions=2, layer1_size=64, layer2_size=64)
+
+
+stepIdx = 0
+currIt = 0
+
+score_history = []
+
+action0_list = []
+obs_list = []
+throughput_history = []
+rtt_history = []
+
+cwnd_history = []
+
+try:
+    for i in range(iterationNum):
+        obs = env.reset()
+
+        obs_list.append(obs[4:])
+
+        reward = 0
+        done = False
+        info = None
+        current_state = [obs[8]*0.00001,obs[9]*0.001,obs[11]*0.000001,obs[13]*(1/340)*0.0001,obs[14]*(1/340)*0.0001]
+        score = 0
+        throughput_sum = 0
+        rtt_sum = 0
+
+        nstep_td_counter = 0
+        accum_reward = 0
+
+        cwnd_history.append(obs[5])
+
+        while True:
+            print("---obs, reward, done, info: ", current_state, reward, done, info)
+            stepIdx += 1
+            throughput_sum += obs[15]
+            rtt_sum += obs[11]
+            print(stepIdx)
+            action = a2c_agent.choose_action(current_state)
+
+            print('CWND increase: {}'.format(action[0]))
+
+            action0_list.append(action[0])
+
+            new_cwnd = obs[5] + action[0]
+            if new_cwnd < 0.0:
+                new_cwnd = 0.0
+            elif new_cwnd > 65535.0:
+                new_cwnd = 65535.0
+
+            new_ssThresh = obs[4] + 0
+            if new_ssThresh < 0:
+                new_ssThresh = 0
+            elif new_ssThresh > 65535:
+                new_ssThresh = 65535
+
+            current_state_utility = math.log1p(obs[15]) - 0.01*math.log1p(obs[11])
+
+            obs, reward, done, info = env.step([new_ssThresh, new_cwnd])
+
+            next_state_utility = math.log1p(obs[15]) - 0.01*math.log1p(obs[11])
+
+            cwnd_history.append(obs[5])
+
+            reward = 0
+            if next_state_utility - current_state_utility >= 0.9:
+                reward = 100
+            elif next_state_utility - current_state_utility <= -0.9:
+                reward = -10
+
+            obs_list.append(obs[4:])
+
+            print('CWND: {}'.format(obs[5]))
+            print('ssThresh: {}'.format(obs[4]))
+
+            next_state = [obs[8]*0.00001,obs[9]*0.001,obs[11]*0.000001,obs[13]*(1/340)*0.0001,obs[14]*(1/340)*0.0001]
+
+            a2c_agent.learn(current_state, reward, next_state, done)
+
+            current_state = next_state
+            score += reward
+
+            if done:
+                stepIdx = 0
+                if currIt + 1 < iterationNum:
+                    env.reset()
+                break
+        throughput_history.append(throughput_sum)
+        rtt_history.append(rtt_sum)
+        score_history.append(score)
+        print('episode: ', i, 'score: %.2f' % score)
+
+
+except KeyboardInterrupt:
+    print("Ctrl-C -> Exit")
+finally:
+    env.close()
+    filename = 'results/td0_TCP_A2C.png'
+    plotLearning(score_history, filename=filename, window=10)
+
+    with open('results/td0_reward.csv', mode='w') as reward_file:
+        csv_writer = csv.writer(reward_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow(score_history)
+
+    throughput_plot_filename = 'results/td0_throughput_plot.png'
+    plotLearning(throughput_history, filename=throughput_plot_filename, window=10)
+
+    rtt_plot_filename = 'results/td0_rtt_plot.png'
+    plotLearning(rtt_history, filename=rtt_plot_filename, window=10)
+
+    print('obs_list: {}'.format(len(obs_list)))
+
+    with open('results/td0_network_obs.csv', mode='w') as network_obs_file:
+        csv_writer = csv.writer(network_obs_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow(['ssThresh','CWND','segmentSize','bytesInFlightSum','bytesInFlightAvg','segmentsAckedSum',
+                             'segmentsAckedAvg','avgRTT','minRTT','avgInterTx','avdInterRx','Throughput'])
+        for i in range(len(obs_list)):
+            csv_writer.writerow(obs_list[i])
+
+    action0_list = sorted(action0_list)
+
+    action0_freq = collections.Counter(action0_list)
+
+    plt.figure()
+    plt.plot(list(action0_freq),list(action0_freq.values()))
+    plt.ylabel('Occurrence')
+    plt.xlabel('Action')
+    plt.savefig('results/td0_action0_occurrence.png')
+
+    episode_number = [i for i in range(len(cwnd_history))]
+    episode_number = [i/1999 for i in episode_number]
+    plt.figure()
+    plt.plot(episode_number, cwnd_history)
+    plt.savefig('results/td0_CWND_Trend.png')
+
+    print("Done")
+
